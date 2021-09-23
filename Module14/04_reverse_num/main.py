87c793026063716a89ac3721ebe4dd354e67be03
Igor Ponomarev <9129910001@mail.ru>
2021-09-23 21:58:07 +0500

    Поправил задание 6 и 7

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -7,49 +7,72 @@ def revers(number):
     return revers_number
 
 
+def symbol(number):
+    number_1 = ''
+    number_2 = ''
+    flag = True
+    for line in number:
+        if flag:
+            if line != '.':
+                number_1 += line
+            elif line == '.':
+                flag = False
+        else:
+            number_2 += line
+            return number_2
+
+
 first_n = input('\nВведите первое число: ')
 second_n = input("\nВведите второе число: ")
+a = first_n = int(first_n)
+print(a)
+b = int(second_n)
 
-# TODO, для сокращения количества кода в текущем задании, предлагаю написать ещё одну функцию.
+
+#  для сокращения количества кода в текущем задании, предлагаю написать ещё одну функцию.
 #  Вне функций должен остаться только код с запросом ввода пользователя и вызовом функций. =)
 
 
-symbol = '.'
-first_number_1 = ''
-first_number_2 = ''
-flag = True
-for line in first_n:
-    if flag:
-        if line != '.':
-            first_number_1 += line
-        elif line == '.':
-            flag = False
-    else:
-        first_number_2 += line
+# symbol = '.'
+# first_number_1 = ''
+# first_number_2 = ''
+# flag = True
+# for line in first_n:
+#     if flag:
+#         if line != '.':
+#             first_number_1 += line
+#         elif line == '.':
+#             flag = False
+#     else:
+#         first_number_2 += line
+#
+# symbol = '.'
+# second_number_1 = ''
+# second_number_2 = ''
+# flag = True
+# for line in second_n:
+#     if flag:
+#         if line != '.':
+#             second_number_1 += line
+#         elif line == '.':
+#             flag = False
+#     else:
+#         second_number_2 += line
 
-symbol = '.'
-second_number_1 = ''
-second_number_2 = ''
-flag = True
-for line in second_n:
-    if flag:
-        if line != '.':
-            second_number_1 += line
-        elif line == '.':
-            flag = False
-    else:
-        second_number_2 += line
+
+# first_number_1 = symbol(first_n)
+first_number_2 = symbol(first_n)
+# second_number_1 = symbol(second_n)
+second_number_2 = symbol(second_n)
+
+first_revers_number_1 = revers(a)
+second_revers_number_1 = revers(b)
 
 
-first_number_1 = int(first_number_1)
-first_number_2 = int(first_number_2)
-second_number_1 = int(second_number_1)
-second_number_2 = int(second_number_2)
+# first_revers_number_1 = revers(first_n)
+# second_revers_number_1 = revers(second_n)
 
-first_revers_number_1 = revers(first_number_1)
 first_revers_number_2 = revers(first_number_2)
-
-second_revers_number_1 = revers(second_number_1)
 second_revers_number_2 = revers(second_number_2)
 
 summa_1 = first_revers_number_1 + second_revers_number_1
--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,32 +1,43 @@
 def special_year(year_1, year_2):
     print('Года от ' + str(year_1) + ' до ' + str(year_2) + ' с тремя одинаковыми цифрами:')
+    a = 0
+    b = 0
+    c = 0
+    d = 0
     for line in range(year_1, year_2 + 1):
-
-        # TODO, предлагаю упростить решение и решить без использования вложенных циклов,
-        #  т.к. каждый цикл создаёт нагрузку на код.
-        #  В таком случае, стоит разбить число на 4 переменные и просто произвести их сравнение при помощи условного
-        #  оператора. Это решение позволит немного ускорить наш код.
-
-        a = line
-        flag = False
-        while a > 0:
-            x = a % 10
-            a = a // 10
-            b = line
-            count = 0
-            while b > 0:
-                y = b % 10
-                b = b // 10
-                if x == y:
-                    count += 1
-            if flag:
-                break
-            else:
-                flag = count == 3
-        if flag:
+        a = line // 1000
+        b = line // 100 % 10
+        c = line // 10 % 10
+        d = line % 10
+        if (a == b == c) or (a == c == d) or (a == b == d) or (b == c == d):
             print(line)
 
 year_1 = int(input('Введите первый год: '))
 year_2 = int(input('Введите второй год: '))
 special_year(year_1, year_2)
 
+
+
+
+
+####################
+#создаёт нагрузку на код
+        # a = line
+        # flag = False
+        # while a > 0:
+        #     x = a % 10
+        #     a = a // 10
+        #     b = line
+        #     count = 0
+        #     while b > 0:
+        #         y = b % 10
+        #         b = b // 10
+        #         if x == y:
+        #             count += 1
+        #     if flag:
+        #         break
+        #     else:
+        #         flag = count == 3
+        # if flag:
+        #     print(line)
+###########################
