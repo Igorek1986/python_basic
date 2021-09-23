43908b5ece08e0eae5ae2c9a662d9e0ce9dab19e
Igor Ponomarev <9129910001@mail.ru>
2021-09-23 16:08:21 +0500

    Задание 7 выполнил

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+def special_year(year_1, year_2):
+    print('Года от ' + str(year_1) + ' до ' + str(year_2) + ' с тремя одинаковыми цифрами:')
+    for line in range(year_1, year_2 + 1):
+        a = line
+        flag = False
+        while a > 0:
+            x = a % 10
+            a = a // 10
+            b = line
+            count = 0
+            while b > 0:
+                y = b % 10
+                b = b // 10
+                if x == y:
+                    count += 1
+            if flag:
+                break
+            else:
+                flag = count == 3
+        if flag:
+            print(line)
+
+year_1 = int(input('Введите первый год: '))
+year_2 = int(input('Введите второй год: '))
+special_year(year_1, year_2)
+
