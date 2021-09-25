a9f4ed66c78479751a7e4f2e4b587e09097068b9
Insolente13 <lanc05@mail.ru>
2021-09-25 11:12:36 +0300

    Module 15 04, 06 - TODO, 01, 02, 03, 05 - ok

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,7 +1,8 @@
 numbers_list = []
 n = int(input('Введите число: '))
 
-
 for i in range(1, n + 1, 2):
     numbers_list.append(i)
 print(numbers_list)
+
+# зачёт!
--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -3,7 +3,9 @@ volleyball_list = ['Артемий', 'Борис', 'Влад', 'Гоша', 'Ди
 for i in range(0, len(volleyball_list), 2):
     print(volleyball_list[i])
 
-
 # это гугл))) Все как в цикле for. start, stop, step
 # b = volleyball_list[::2]
 # print(b)
+
+# Верно! До срезов дойдём в 17 уроке =)
+# зачёт!
