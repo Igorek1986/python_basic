71add806e53794125de66026fa4ee59d38554de6
Igor Ponomarev <9129910001@mail.ru>
2021-09-25 14:37:56 +0500

    Task 6 update.

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -6,12 +6,11 @@ num_max = 0
 for i in range(number):
     graphics_card = int(input(str(i + 1) + ' Видеокарта: '))
     NVIDIA_list.append(graphics_card)
-    # TODO, для сокращения количества вычислений по индексам, в коде ниже,
-    #  предлагаю сравнивать максимальное число с вводом пользователя graphics_card.
-    if num_max <= NVIDIA_list[i]:
-        num_max = NVIDIA_list[i]
+    if num_max <= graphics_card:
+        num_max = graphics_card
 
-# TODO, предлагаю уйти от вычислений элементов по их индексам и в цикле ниже, идти сразу по списку видеокарт. =)
+#  предлагаю уйти от вычислений элементов по их индексам и в цикле ниже, идти сразу по списку видеокарт. =)
+
 for i in range(number):
     if NVIDIA_list[i] != num_max:
         new_NVIDIA_list.append(NVIDIA_list[i])
