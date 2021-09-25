56db30405c3292d93f25fc7031378192869bb0bd
Igor Ponomarev <9129910001@mail.ru>
2021-09-25 19:21:38 +0500

    Task 10 done

---
+++ Module15/10_sort/main.py
@@ -0,0 +1,8 @@
+unsorted_list = [1, 4, -3, 0, 10]
+print('Изначальный список:', unsorted_list)
+for i in range(len(unsorted_list) - 1):
+    for i2 in range(len(unsorted_list) - 1 - i):
+        if unsorted_list[i2] > unsorted_list[i2 + 1]:
+            unsorted_list[i2], unsorted_list[i2 + 1] = unsorted_list[i2 + 1], unsorted_list[i2]
+
+print('Отсортированный список:', unsorted_list)
