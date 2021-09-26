64c4a27bb51097a1f8aecb5ed86d8b287b76bd39
Igor Ponomarev <9129910001@mail.ru>
2021-09-26 23:35:07 +0500

    Branch to branch Module16

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,43 +1,29 @@
-def deletion(num_list, num):
-    for i in num_list:
-        if i == num:
-            a.remove(num)
-
-def added(num_list):
-    a.extend(num_list)
-
 a = [1, 5, 3]
 b = [1, 5, 1, 5]
 c = [1, 3, 1, 5, 3, 3]
 
-added(b)
 
-print('Кол-во цифр 5 при первом объединении:', a.count(5))
+for i in b:
+    a.append(i)
+t = 0
+for i in a:
+    if i == 5:
+        t += 1
+print(t)
+d = []
+for i in a:
+    if i != 5:
+        d.append(i)
+for i in c:
+    d.append(i)
+t = 0
+for i in d:
+    if i == 3:
+        t += 1
+print(t)
+print(d)
 
-deletion(a, 5)
-added(c)
+# TODO переписать программу
 
-print('Кол-во цифр 3 при втором объединении:', a.count(3))
-print('Итоговый список:', a)
-#
-# # for i in b:
-# #     a.append(i)
-# # t = 0
-# # for i in a:
-# #     if i == 5:
-# #         t += 1
-# # print(t)
-# d = []
-# for i in a:
-#     if i != 5:
-#         d.append(i)
-# for i in c:
-#     d.append(i)
-# t = 0
-# for i in d:
-#     if i == 3:
-#         t += 1
-# print(t)
-# print(d)
 
 
--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,30 +1,3 @@
-def is_guests_exist(guest, guests_list):
-    for i_guest in guests_list:
-        if i_guest != guest:
-            return True
-    return False
-
-
 guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
 
-
-while True:
-    print('Сейчас на вечеринке', len(guests), 'человек:', guests)
-    new_guests = input('Гость пришел или ушел? пришел\nИмя гостя: ')
-    if len(guests) <=6:
-        if is_guests_exist(new_guests, guests):
-            command = input('Введите команду: ')
-            if command == 'пришел':
-                if is_guests_exist(new_guests, guests):
-                    print('Человек с таким именем уже есть. Добавьте первую букву фамилии.\n')
-                else:
-                    guests.append(new_guests)
-                    print('Привет, ' + str(new_guests) + '!\n')
-            if command == 'ушел':
-                if is_guests_exist(new_guests, guests):
-                    print('Человека с таким именем нет на вечеринке.')
-                else:
-                    guests.remove(new_guests)
-                    print('Пока', new_guests)
-    else:
-        print('Прости', new_guests, 'но мест нет.\n')
+# TODO здесь писать код
