b1694738a5b48767c89b77d06b4514c4663a4c50
Insolente13 <lanc05@mail.ru>
2021-09-27 19:51:55 +0300

    Module 16 03 - TODO, 01, 02 - ok

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -11,7 +11,6 @@ main_list = [1, 5, 3]
 side_list_1 = [1, 5, 1, 5]
 side_list_2 = [1, 3, 1, 5, 3, 3]
 
-
 added(side_list_1)
 
 print('Кол-во цифр 5 при первом объединении:', main_list.count(5))
@@ -41,3 +40,5 @@ print('Итоговый список:', main_list)
 #         t += 1
 # print(t)
 # print(d)
+
+# зачёт!
