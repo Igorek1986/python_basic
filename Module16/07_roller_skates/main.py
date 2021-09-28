42864226d799c87475ef989f85f03b9940627824
Igor Ponomarev <9129910001@mail.ru>
2021-09-28 23:01:44 +0500

    Task 6 update, 7 done

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -1,9 +1,24 @@
-skates_size = [41, 40, 39, 42]
-foot_size = [42, 41, 42]
-count = 0
-for i in skates_size:
-    for i2 in foot_size:
-        if i == i2:
-            count += 1
+skates_size = []
+foot_size = []
+count_skates = int(input('Кол-во коньков: '))
 
-print(count)
+
+for i_skates in range(count_skates):
+    skates_size.append(int(input('Размер ' + str(i_skates + 1) + ' пары: ')))
+
+
+count_people = int(input('\nКол-во людей: '))
+
+
+for i_people in range(count_people):
+    foot_size.append(int(input('Размер ' + str(i_people + 1) + ' ноги: ')))
+
+
+for i in range(len(foot_size)):
+    a = foot_size[i]
+    for i2 in range(len(skates_size)):
+        b = skates_size[i2]
+        if a == b:
+            skates_size.remove(skates_size[i2])
+            break
+print('\nНаибольшее кол-во людей, которые могут взять ролики:', count_skates - len(skates_size))
