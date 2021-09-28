3036d32ca3575eae517e7b026114c3c7fdf5b759
Insolente13 <lanc05@mail.ru>
2021-09-28 21:40:50 +0300

    Module 16 07  - TODO, 01, 02, 03, 04, 05, 06 - ok

--- Module16/06_unique_elems/main.py
+++ Module16/06_unique_elems/main.py
@@ -1,7 +1,6 @@
 first_list = [1, 2, 3]
 second_list = [2, 4, 6, 3, 3, 2, 1]
 
-
 # first_list = []
 # second_list = []
 #
@@ -18,6 +17,6 @@ for i in first_list:
     for i2 in range(first_list.count(i) - 1):
         first_list.remove(i)
 
-
 print('Новый первый список с уникальными элементами:', first_list)
 
+# зачёт!
--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -2,7 +2,8 @@ skates_size = []
 foot_size = []
 count_skates = int(input('Кол-во коньков: '))
 
-
+# TODO, как реализовать range таким образом, чтобы не производить в цикле вычисления (+1) с переменной цикла?
+#  В таком случае, вычисление произойдёт только 1 раз в range, вместо вычислений каждую итерацию цикла.
 for i_skates in range(count_skates):
     skates_size.append(int(input('Размер ' + str(i_skates + 1) + ' пары: ')))
 
@@ -18,7 +19,7 @@ for i in range(len(foot_size)):
     a = foot_size[i]
     for i2 in range(len(skates_size)):
         b = skates_size[i2]
-        if a == b:
-            skates_size.remove(skates_size[i2])
+        if a == b:  # TODO, размер коньков может быть больше размера ноги человека =)
+            skates_size.remove(skates_size[i2])  # TODO, удалять можно сразу переменную "b".
             break
 print('\nНаибольшее кол-во людей, которые могут взять ролики:', count_skates - len(skates_size))
