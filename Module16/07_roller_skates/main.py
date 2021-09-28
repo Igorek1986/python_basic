9a1ea7b1080d37b757b0b9232aafe72afb4b71c9
Igor Ponomarev <9129910001@mail.ru>
2021-09-29 00:50:10 +0500

    Task 7 update, 8 done

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -16,10 +16,8 @@ for i_people in range(count_people):
 
 
 for i in range(len(foot_size)):
-    a = foot_size[i]
     for i2 in range(len(skates_size)):
-        b = skates_size[i2]
-        if a == b:  # TODO, размер коньков может быть больше размера ноги человека =)
-            skates_size.remove(skates_size[i2])  # TODO, удалять можно сразу переменную "b".
+        if foot_size[i] <= skates_size[i2]:
+            skates_size.remove(skates_size[i2])
             break
 print('\nНаибольшее кол-во людей, которые могут взять ролики:', count_skates - len(skates_size))
--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+count_number = int(input('Какое число в считалке? '))
+count_people = int(input('Кол-во человек: '))
+people = list(range(1, count_people + 1))
+print('Значит, выбывает каждый', count_number, 'человек\n')
+out_circle = 0
+
+
+for i in range(count_people - 1):
+    print('Текущий круг людей: ', people)
+    start = out_circle % len(people)
+    out_circle = (start + count_number - 1) % len(people)
+    print('Начало счёта с номера', people[start])
+    print('Выбывает человек под номером', people[out_circle], '\n')
+    people.remove(people[out_circle])
