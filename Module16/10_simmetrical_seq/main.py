e1e7f44ea22ca8749c4f758f65b2a34664c51acc
Igor Ponomarev <9129910001@mail.ru>
2021-09-29 22:25:44 +0500

    task 10 draft

--- Module16/10_simmetrical_seq/main.py
+++ Module16/10_simmetrical_seq/main.py
@@ -1,1 +1,33 @@
-# TODO здесь писать код
+def reverse(num_list):
+    index = len(num_list) - 1
+    for i in range((len(num_list) - 1) // 2):
+        temp = num_list[index]
+        num_list[index] = num_list[i]
+        num_list[i] = temp
+        index -= 1
+        return num_list
+
+
+count = int(input('Длинна списка: '))
+number_list = []
+revers_list = []
+
+for i in range(count):
+    number = int(input('Введите число: '))
+    number_list.append(number)
+
+revers_list.extend(number_list)
+revers_list = reverse(revers_list)
+
+
+print(number_list)
+print(revers_list)
+
+
+for i in range(1, 5):
+    if number_list[-i] == revers_list[i]:
+        print('число', i)
+        break
+    else:
+        number_list.remove(number_list[i])
+        revers_list.remove(revers_list[i])
