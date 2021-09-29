c2d16dbe13affa15988a63a0de495c84468715cc
Igor Ponomarev <9129910001@mail.ru>
2021-09-29 18:38:42 +0500

    Task 9 done

--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,10 +1,18 @@
 count_friends = int(input('Кол-во друзей: '))
 count_debt = int(input('Долговых расписок: '))
-friend_list = []
+friend_list = [0, 0, 0]
 
-for _ in range(0, count_debt + 1):
-    print('\n', count_debt, 'расписка')
+for i in range(1, count_debt + 1):
+    print()
+    print(i, 'расписка')
     to_people = int(input('Кому: '))
     from_people = int(input('От кого: '))
-    money = int(input('Сколько: '))
-    if to_people == 1:
+    if to_people != from_people:
+        money = int(input('Сколько: '))
+        friend_list[to_people - 1] = -money + friend_list[to_people - 1]
+        friend_list[from_people - 1] = money + friend_list[from_people - 1]
+    else:
+        print('Самому себе занимать нельзя!')
+print('\nБаланс друзей:')
+for i in range(1, count_friends + 1):
+    print(str(i), ' :', friend_list[i - 1])
