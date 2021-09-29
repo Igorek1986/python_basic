c5a4e163c5aadfa019ca3b7e9cb3ebf34d291238
Igor Ponomarev <9129910001@mail.ru>
2021-09-29 06:20:06 +0500

    Task 7 update

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -2,17 +2,16 @@ skates_size = []
 foot_size = []
 count_skates = int(input('Кол-во коньков: '))
 
-# TODO, как реализовать range таким образом, чтобы не производить в цикле вычисления (+1) с переменной цикла?
-#  В таком случае, вычисление произойдёт только 1 раз в range, вместо вычислений каждую итерацию цикла.
-for i_skates in range(count_skates):
-    skates_size.append(int(input('Размер ' + str(i_skates + 1) + ' пары: ')))
+
+for i_skates in range(1, count_skates + 1):
+    skates_size.append(int(input('Размер ' + str(i_skates) + ' пары: ')))
 
 
 count_people = int(input('\nКол-во людей: '))
 
 
-for i_people in range(count_people):
-    foot_size.append(int(input('Размер ' + str(i_people + 1) + ' ноги: ')))
+for i_people in range(1, count_people + 1):
+    foot_size.append(int(input('Размер ' + str(i_people) + ' ноги: ')))
 
 
 for i in range(len(foot_size)):
@@ -21,4 +20,3 @@ for i in range(len(foot_size)):
             skates_size.remove(skates_size[i2])
             break
 print('\nНаибольшее кол-во людей, которые могут взять ролики:', count_skates - len(skates_size))
-
--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+count_friends = int(input('Кол-во друзей: '))
+count_debt = int(input('Долговых расписок: '))
+friend_list = []
+
+for _ in range(0, count_debt + 1):
+    print('\n', count_debt, 'расписка')
+    to_people = int(input('Кому: '))
+    from_people = int(input('От кого: '))
+    money = int(input('Сколько: '))
+    if to_people == 1:
