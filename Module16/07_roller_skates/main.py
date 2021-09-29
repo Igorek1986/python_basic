24ab21598d0971a84debf2de3efcffae61ecb5b7
Insolente13 <lanc05@mail.ru>
2021-09-29 09:23:56 +0300

    Module 16 01, 02, 03, 04, 05, 06, 07, 08 - ok

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -2,17 +2,13 @@ skates_size = []
 foot_size = []
 count_skates = int(input('Кол-во коньков: '))
 
-
 for i_skates in range(1, count_skates + 1):
     skates_size.append(int(input('Размер ' + str(i_skates) + ' пары: ')))
 
-
 count_people = int(input('\nКол-во людей: '))
-
 
 for i_people in range(1, count_people + 1):
     foot_size.append(int(input('Размер ' + str(i_people) + ' ноги: ')))
-
 
 for i in range(len(foot_size)):
     for i2 in range(len(skates_size)):
@@ -20,3 +16,5 @@ for i in range(len(foot_size)):
             skates_size.remove(skates_size[i2])
             break
 print('\nНаибольшее кол-во людей, которые могут взять ролики:', count_skates - len(skates_size))
+
+# зачёт!
