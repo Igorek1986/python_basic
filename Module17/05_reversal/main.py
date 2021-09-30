8f6ec99a23156ff34da7293d82c42b347b69c1ae
Igor Ponomarev <9129910001@mail.ru>
2021-10-01 00:24:13 +0500

    Task 1-5 done

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+word = 'dferghewrterglheteteht'
+
+
+word_new = [word[i+1:] for i in range(len(word)) if word[i] == 'h']
+word_new = word_new[0]
+word_new = [word[:i+1] for i in range(len(word_new), 0, -1) if word[i] == 'h']
+word_new = word_new[0]
+
+
+print('Слово: ', word)
+print('Разворот между первой h и последней: ', word_new[::-1])
