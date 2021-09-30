6d16904bb8ba89307ad10652c23b8d737cdacdec
Igor Ponomarev <9129910001@mail.ru>
2021-09-30 18:43:20 +0500

    Task 10 done

--- Module16/10_simmetrical_seq/main.py
+++ Module16/10_simmetrical_seq/main.py
@@ -8,26 +8,45 @@ def reverse(num_list):
         return num_list
 
 
+def ans_print(num_list):
+    for i in range(len(num_list)):
+        print(num_list[i], end=' ')
+
+
 count = int(input('Длинна списка: '))
 number_list = []
 revers_list = []
+answer_list = []
+
 
 for i in range(count):
     number = int(input('Введите число: '))
     number_list.append(number)
 
+
 revers_list.extend(number_list)
 revers_list = reverse(revers_list)
 
 
-print(number_list)
-print(revers_list)
+print('\nПоследовательность: ', end='')
+ans_print(number_list)
 
 
-for i in range(1, 5):
-    if number_list[-i] == revers_list[i]:
-        print('число', i)
-        break
-    else:
-        number_list.remove(number_list[i])
-        revers_list.remove(revers_list[i])
+for i in range(len(number_list)):
+    for _ in range(len(revers_list)):
+        if number_list == revers_list:
+            break
+        else:
+            answer_list.append(number_list[i])
+            number_list.remove(number_list[0])
+            revers_list.remove(revers_list[-1])
+
+
+reverse(answer_list)
+
+
+print('\nНужно приписать чисел:', number_list[0])
+
+
+print('Сами числа:', end=' ')
+ans_print(answer_list)
