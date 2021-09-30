5b7265d548455a15ec5281f14911205ba00ba69b
Igor Ponomarev <9129910001@mail.ru>
2021-09-30 19:39:30 +0500

    Added method from parsing

--- Module16/10_simmetrical_seq/main.py
+++ Module16/10_simmetrical_seq/main.py
@@ -45,3 +45,34 @@ print('Сами числа:', end=' ')
 ans_print(answer_list)
 
 # зачёт!
+
+# #############
+# #Разбор ДЗ
+# def is_palindrome(num_list):
+#     revers_list = []
+#     for i_num in range(len(num_list) -1, -1, -1):
+#         revers_list.append(num_list[i_num])
+#     if num_list == revers_list
+#         return True
+#     else:
+#         return False
+#
+# nums = [1, 2, 3, 4, 5]
+# new_nums = []
+# answer = []
+#
+# for i_nums in range(0, len(nums)):
+#     for j_elem in range(i_nums, len(nums)):
+#         new_nums.append(nums[j_elem])
+#     if is_palindrome(new_nums):
+#         for i_answer in range(0, i_nums):
+#             answer.append(nums[i_answer])
+#             answer.reverse()
+#             break
+#     new_nums = []
+#
+# print('Последовательность: ', nums)
+# print('Нужно приписать чисел:', len(answer))
+# print('Сами числа:', answer)
+#
+# #############
