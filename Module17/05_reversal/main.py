d76941aaa952d9092ecc947f0e38d4496c623738
Igor Ponomarev <9129910001@mail.ru>
2021-10-01 06:22:38 +0500

    Task 5 update

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,18 +1,5 @@
-# word = 'dfergHewrterglHeteteHt'
 word = 'ooohokihkkk'
-
-# TODO, пожалуйста, обратите внимание, циклы в данном задании не нужны,
-#  предлагаю попробовать решить только при помощи поиска элементов по индексу и срезам.
-#  Ознакомиться с методами index и rindex, и их параметрами можно:
-#  https://pythonz.net/references/named/str.rindex/
-#  https://pythonz.net/references/named/str.index/
-word = 'dferghewrterglheteteht'
-
-
-word_new = [word[i+1:] for i in range(len(word)) if word[i] == 'h']
-word_new = word_new[0]
-word_new = [word[:i+1] for i in range(len(word_new), 0, -1) if word[i] == 'h']
-word_new = word_new[0]
+word_new = word[word.index('h') + 1:word.rindex('h')]
 
 
 print('Слово: ', word)
