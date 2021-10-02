48ca31d0c7153bf8c91bbd46f300f8cccb14f37b
Igor Ponomarev <9129910001@mail.ru>
2021-10-02 09:47:47 +0500

    Task 5 update, task 6-7 done

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -4,6 +4,3 @@ word_new = word[:word.index('h')] + word[word.rindex('h'):word.index('h'):-1] + 
 
 print('Слово: ', word)
 print('Разворот между первой h и последней: ', word_new) #Можно не делать word_new, сразу заменить в word
-
-
-
