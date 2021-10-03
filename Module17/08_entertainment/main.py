48a7e8ce94da396214f67e981c37eb9311eec6a4
Igor Ponomarev <9129910001@mail.ru>
2021-10-03 16:12:47 +0500

    Task 9 done

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import random
+
+num_sticks = 10 #int(input('Кол-во палок: '))
+num_throw = 3 #int(input('Кол-во бросков: '))
+# L_i, R_i = [random.randint(1, R_i)], [random.randint(L_i, num_sticks)]
+
+# print(L_i)
+# print(R_i)
+#1≤ L_i≤ R_i≤ N
--- Module17/09_list_of_lists_2/main.py
+++ Module17/09_list_of_lists_2/main.py
@@ -1,4 +1,5 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+nice_list = [i for i3 in nice_list for i2 in i3 for i in i2]
+print(nice_list)
