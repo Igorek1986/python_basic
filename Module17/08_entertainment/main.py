fdfcb10bbdd40400c6767bb9f916fb66daf682cc
Igor Ponomarev <9129910001@mail.ru>
2021-10-04 23:35:59 +0500

    Task 8,10 update

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -7,8 +7,8 @@ num_throw = 3 #int(input('Кол-во бросков: '))
 
 R_i = [random.randint(1, num_sticks) for _ in range(num_throw)]
 L_i = [random.randint(1, num_sticks) for _ in range(num_throw)]
-result = ['I' for _ in range(num_sticks)]
-
+result_list = ['I' for _ in range(num_sticks)]
+result = ''
 
 L_i, R_i = [R_i[i] if R_i[i] < L_i[i] else L_i[i] for i in range(len(L_i))], \
            [L_i[i] if R_i[i] < L_i[i] else R_i[i] for i in range(len(R_i))]
@@ -20,9 +20,11 @@ for i in range(num_throw):
     left = int(L_i[i])
     right = int(R_i[i])
     for i2 in range(left - 1, right):
-        result[i2] = '.'
+        result_list[i2] = '.'
 
 
-print(result)
+for i in range(len(result_list)):
+    result += result_list[i]
 
-# TODO, пожалуйста, реализуйте вывод как в примере. =)
+
+print('\nРезультат:', result)
