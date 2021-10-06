3453b0f0e183497dc0636cc8a200ee93ac2d7c2c
Igor Ponomarev <9129910001@mail.ru>
2021-10-06 22:48:37 +0500

    Task 5 question

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,9 +1,10 @@
+def check_password(passwd):
+    return not ((len(passwd) < 8) or passwd.isdigit() or passwd.isalpha() or passwd.islower() or passwd.isupper())
+
+
 password = input('Придумайте пароль: ')
 while True:
-    print(not password.islower() < 1)
-    print(not password.isupper() < 1)
-    print(not password.isdigit() < 3)
-    if not password.islower() < 1 and not password.isupper() < 1 and not password.isdigit() > 2 and len(password) > 8:
+    if check_password(password):
         print('Это надёжный пароль!')
         break
     else:
