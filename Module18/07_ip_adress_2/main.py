02578fd6c956d2c128d6a301a09962c1ec7660c6
Igor Ponomarev <9129910001@mail.ru>
2021-10-07 00:23:42 +0500

    Task 7 done

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+def check_ip(address):
+    parts = address.split(".")
+    if len(parts) != 4:
+        print('Адрес - это четыре числа, разделенные точками')
+        return
+    for i in parts:
+        if not i.isdigit():
+            print(i + '- не целое число')
+            return
+    for item in parts:
+        if not 0 <= int(item) <= 255:
+            print(item, 'превышает 255')
+            return
+    print('IP-адрес корректен')
+
+
+ip_adress = '128.16.35.255'
+check_ip(ip_adress)
