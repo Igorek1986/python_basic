84b4c4cfd2a9c1c68a710bc317d0ac286852e589
Igor Ponomarev <9129910001@mail.ru>
2021-10-07 21:54:59 +0500

    Task 9 question

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+# message = 'Это задание очень! простое.'.split()
+message = 'Хотя ,. возм:ожно и нет.'.split()
+
+# message = message.split()
+print(message)
+word = ''
+for i in range(len(message)):
+    if message[i].isalpha():
+        message[i] = message[i][::-1]
+    else:
+        for i2 in message[i]:
+            if i2.isalpha():
+                word = i2 + word
+            else:
+                word = word + i2
+        message[i] = word
+        word = ''
+
+print(message)
