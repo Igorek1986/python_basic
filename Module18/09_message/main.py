bea9baba65342914073e610100b813db13a29360
Insolente13 <lanc05@mail.ru>
2021-10-08 00:49:37 +0300

    Module 18 10 - not yet, another - ok

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,7 +1,6 @@
 # message = 'Это задание очень! простое.'.split()
 message = 'Хотя ,. возм:ожно и нет.'.split()
 
-
 print('Сообщение:', ' '.join(message))
 test = ''
 for i in range(len(message)):
@@ -10,8 +9,9 @@ for i in range(len(message)):
     else:
         for i2 in message[i]:
             if not i2.isalpha():
-                message[i] = message[i][:message[i].index(i2):1][::-1] + i2\
+                message[i] = message[i][:message[i].index(i2):1][::-1] + i2 \
                              + message[i][1 + message[i].index(i2)::1][::-1]
 
-
 print('Новое сообщение:', ' '.join(message))
+
+# зачёт!
