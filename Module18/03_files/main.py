5ac6291b326ab37da0c501141d2128bb3ac426a1
Igor Ponomarev <9129910001@mail.ru>
2021-10-07 20:23:32 +0500

    Task 3 update, task 8 done

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,19 +1,16 @@
+def check_name(file, start_f):
+    for i in start_f:
+        if file.startswith(i):
+            print('Ошибка: название начинается на один из специальных символов')
+            return
+    if file.endswith('.txt') or file.endswith('.docx'):
+        print('Файл назван верно.')
+    else:
+        print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx')
+
+
 name_file = input('Название файла: ')
 start_file = '@№$%^&*'
 
 
-for i in start_file:
-    if name_file.startswith(i):
-        print(name_file.startswith(i))
-        print('Ошибка: название начинается на один из специальных символов')
-        #break
-    # TODO, пожалуйста, обратите внимание, действия ниже, в текущем цикле, получились лишними.
-    #  Условие из блока elif, необходимо проверить только один раз.
-
-    # if not name_file.endswith('.txt'):
-    #     print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx')
-    # elif not name_file.endswith('.docx'):
-    #     print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx')
-    # else:
-    #     print('Файл назван верно.')
-
+check_name(name_file, start_file)
