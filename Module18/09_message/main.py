d08524f421928677aaf8b2d4a0759fdf80aae3dc
Igor Ponomarev <9129910001@mail.ru>
2021-10-08 01:21:52 +0500

    Task 9 done

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,28 +1,17 @@
-message = 'Это задание очень! простое.'.split()
-# message = 'Хотя ,. возм:ожно и нет.'.split()
+# message = 'Это задание очень! простое.'.split()
+message = 'Хотя ,. возм:ожно и нет.'.split()
 
-# message = message.split()
-print(message)
+
+print('Сообщение:', ' '.join(message))
 test = ''
 for i in range(len(message)):
     if message[i].isalpha():
         message[i] = message[i][::-1]
     else:
         for i2 in message[i]:
+            if not i2.isalpha():
+                message[i] = message[i][:message[i].index(i2):1][::-1] + i2\
+                             + message[i][1 + message[i].index(i2)::1][::-1]
 
-            # TODO, в текущем цикле, будет достаточно одного блока if с проверкой, является ли i2 буквой.
-            #  Нам необходимо найти символ, который буквой не является, чтобы подобрать корректный срез исходя
-            #  из индекса этого символа в слове.
-            #  Таким образом, чтобы из "возм:ожно" получилось "мзов:онжо". =)
 
-            # print(i2)
-            message[i] = test
-            if i2.isalpha():
-                test = i2 + test
-                # print(test)
-            elif not i2.isalpha() and not ' ':
-                test = test + i2
-            else:
-                test = ' '
-
-print(message)
+print('Новое сообщение:', ' '.join(message))
