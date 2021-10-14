5a8253baaed410177886b9db96e5b0bcc4321253
Igor Ponomarev <9129910001@mail.ru>
2021-10-14 11:51:28 +0500

    Task 08 update

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,26 +1,22 @@
 max_number = int(input('Введите максимальное число: '))
-
 guess_number = set({str(x) for x in range(1, max_number + 1)})
-numbers = input('\nНужное число есть среди вот этих чисел: ').split()
-person_number = set(numbers)
-
-# TODO Предлагаю сократить количество повторяющихся строк кода с запросом ввода пользователя до одной.
-#  Стоит попробовать реализовать цикл while True и запрашивать данные только в цикле.
-#  В текущей реализации нарушается принцип программирования DRY (Don`t Repeat yourself).
 
 
-while numbers != 'Помогите!':
-    answer = input('Ответ Артёма: ')
-    if answer == 'Yes':
-        guess_number = guess_number.intersection(person_number)
-        numbers = input('\nНужное число есть среди вот этих чисел: ').split()
-        person_number = set(numbers)
-    elif answer == 'No':
-        guess_number = guess_number.difference(person_number)
-        numbers = input('\nНужное число есть среди вот этих чисел: ')
-        person_number = set(numbers)
+while True:
+    numbers = set(input('\nНужное число есть среди вот этих чисел: ').split())
+    if numbers != set('Помогите!'.split()):
+        answer = input('Ответ Артёма: ')
+        if answer == 'Yes':
+            guess_number = guess_number.intersection(numbers)
+            if len(guess_number) == 1:
+                break
+        elif answer == 'No':
+            guess_number = guess_number.difference(numbers)
+        else:
+            print('\nОшибка ввода Артема. Ответ должен быть Yes or No')
     else:
-        print('\nОшибка ввода Артема. Ответ должен быть Yes or No')
+        break
+
 
 print('\nАртём мог загадать следующие числа:', end=' ')
 for num in sorted(guess_number):
@@ -40,7 +36,6 @@ for num in sorted(guess_number):
 # Нужное число есть среди вот этих чисел: 4
 # Ответ Артёма: Yes
 #
-# TODO, по идее "Помогите!" мы ждём от пользователя => numbers может быть или числом или текстом. =)
 # Нужное число есть среди вот этих чисел: Помогите!
 # Ответ Артёма: Помогите!
 #
