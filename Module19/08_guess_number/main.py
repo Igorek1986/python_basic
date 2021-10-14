920075ee6f73923c856a0d3f6d18ac0861f3c1c1
Igor Ponomarev <9129910001@mail.ru>
2021-10-14 10:01:14 +0500

    Task 08 done

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+max_number = int(input('Введите максимальное число: '))
+
+
+guess_number = set({str(x) for x in range(1, max_number + 1)})
+numbers = input('\nНужное число есть среди вот этих чисел: ').split()
+person_number = set(numbers)
+
+
+while numbers != 'Помогите!':
+    answer = input('Ответ Артёма: ')
+    if answer == 'Yes':
+        guess_number = guess_number.intersection(person_number)
+        numbers = input('\nНужное число есть среди вот этих чисел: ').split()
+        person_number = set(numbers)
+    elif answer == 'No':
+        guess_number = guess_number.difference(person_number)
+        numbers = input('\nНужное число есть среди вот этих чисел: ')
+        person_number = set(numbers)
+    else:
+        print('\nОшибка ввода Артема. Ответ должен быть Yes or No')
+
+print('\nАртём мог загадать следующие числа:', end=' ')
+for num in sorted(guess_number):
+    print(num, end=' ')
