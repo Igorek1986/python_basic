ed808a0be5c40967b7d5cdabd74775207e312240
Igor Ponomarev <9129910001@mail.ru>
2021-10-14 19:06:55 +0500

    Task 09 question

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+numbers = int(input('Введите количество человек: '))
+pedigree = dict()
+
+for i_numbers in range(1, numbers):
+    tree_element = input(f'{i_numbers} пара: ').split()
+    child, parent = tree_element[0], tree_element[1]
+    if parent in pedigree:
+        pedigree[parent][child] = None
+        if parent in pedigree[parent]:
+            pedigree[parent][child] = None
+        else:
+            pedigree[parent] = {child: None}
+    else:
+        pedigree[parent] = {child: None}
+
+print(pedigree)
