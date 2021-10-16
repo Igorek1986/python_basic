d8f6da219b496462a392ea8405e17c00e28fde2c
Igor Ponomarev <9129910001@mail.ru>
2021-10-16 10:03:04 +0500

    Task 10 done

--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+def palindrome(string):
+    count_letter = 1
+    count = 0
+    for i_string in string:
+        if i_string in string_dict:
+            count_letter += 1
+            string_dict[i_string] = count_letter
+        else:
+            string_dict[i_string] = 1
+    for i_count in string_dict.values():
+        if i_count % 2 == 1:
+            count += 1
+    if count > 1:
+        return False
+    else:
+        return True
+
+
+while True:
+    string_lst = list(input('Введите строку: '))
+    string_dict = dict()
+    palindrome(string_lst)
+    if palindrome(string_lst):
+        print('\nМожно сделать палиндромом\nДля выхода введите stop\n')
+    elif string_lst == list('stop'):
+        break
+    else:
+        print('\nНельзя сделать палиндромом\nДля выхода введите stop\n')
