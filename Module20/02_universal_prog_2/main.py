d8014dd970474e7bfeebdc2f03848b11d3ed9223
Igor Ponomarev <9129910001@mail.ru>
2021-10-18 08:49:59 +0500

    Task 02 update Task 01 done

--- Module20/02_universal_prog_2/main.py
+++ Module20/02_universal_prog_2/main.py
@@ -1,1 +1,31 @@
-# TODO здесь писать код
+def is_prime(number):
+    if number < 2:
+        return False
+    elif number == 2:
+        return True
+    for i_num in range(2, number):
+        if number % i_num == 0:
+            return False
+    return True
+
+
+def i_is_prime(data):
+    if not isinstance(data, dict):
+        for i in enumerate(data):
+            if is_prime(i[0]):
+                lst_elements.append(i[1])
+    else:
+        for i in data.items():
+            if is_prime(i[0]):
+                lst_elements.append(i[1])
+
+
+# object_elements = (0, 1, 2, 3, 4, 5)
+# object_elements = ('0123456789')
+# object_elements = [0, 1, 2, 3, 4, 5]
+object_elements = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 17: 7}
+
+
+lst_elements = []
+i_is_prime(object_elements)
+print(lst_elements)
