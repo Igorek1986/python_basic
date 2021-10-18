88329edc9872a476a2935b2548e5bee1a1478166
Igor Ponomarev <9129910001@mail.ru>
2021-10-18 23:03:32 +0500

    Task 10 done

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+def my_len_min(string, data):
+    return len(sorted((string, data), key=len)[0])
+
+
+word_str = 'abcd'
+num = (10, 20, 30, 40)
+min_len = my_len_min(word_str, num)
+
+
+gen_obj = ((word_str[i], num[i]) for i in range(min_len))
+print(gen_obj)
+
+for item in gen_obj:
+    print(item)
+
+
+# sym_num = zip(word_str, num)
+# print(sym_num)
+#
+#
+# for i in sym_num:
+#     print(i)
