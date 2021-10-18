eb17d307c9faa4d41f79df701100ebbc7614596a
Igor Ponomarev <9129910001@mail.ru>
2021-10-18 21:55:58 +0500

    Task 06, 07, 08 done

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def sort(tuple_data):
+    flag = True
+    for i in tuple_data:
+        if not type(i) is int:
+            flag = False
+    if flag:
+        sorted_tuple = sorted(tuple_data)
+        return sorted_tuple
+    else:
+        return tuple_data
+
+
+original_tuple = (3, 5, 1, 4)
+print(sort(original_tuple))
--- Module20/08_contacts_3/main.py
+++ Module20/08_contacts_3/main.py
@@ -1,1 +1,37 @@
-# TODO здесь писать код
+def add_book(p_dict, string):
+    surname_name = tuple(string[:2])
+    tel = string[2]
+    if surname_name in p_dict:
+        print('Такая фамилия и имя уже есть в БД. Запись не произведена!')
+    else:
+        p_dict[surname_name] = tel
+        for i_person in p_dict:
+            print(f'{i_person[0]} {i_person[1]}: {p_dict[i_person]}')
+
+
+def search_people(surname):
+    if surname[-1] == 'а':
+        surname = surname[:-1]
+    for people, age in phonebook.items():
+        if surname in people[0]:
+            print(people[0], people[1], age)
+
+
+phonebook = {}
+
+
+while True:
+    action = input('\n"добавить контакт" или "найти человека", '
+                   '"стоп" для выхода: ').lower()
+    if action == 'добавить контакт':
+        person = input('Введите Фамилию Имя и телефон через пробел: ').title().split()
+        print('')
+        add_book(phonebook, person)
+    elif action == 'найти человека':
+        surname_search = input('Введите фамилию для поиска: ').title()
+        search_people(surname_search)
+    elif action == 'стоп':
+        print('До свидания!')
+        break
+    else:
+        print('Ошибка ввода команды!')
