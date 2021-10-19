9ebac889d2f2cdb18df3e060e0abd04f4c888085
Igor Ponomarev <9129910001@mail.ru>
2021-10-19 20:08:47 +0500

    Task 09 done

--- Module20/09_competition_protocol/main.py
+++ Module20/09_competition_protocol/main.py
@@ -1,38 +1,31 @@
-def winner_sort(scores, num):
-    test = num * 100000000 - scores
-    return test
+def winner_sort(winner):
+    return winner[1][0] * 100000000 - winner[1][1]
 
 
-# count_protocol = int(input('Сколько записей вносится в протокол? '))
-# games = {}
-#
-# print('Записи (результат и имя): ')
-# for i_protocol in range(count_protocol):
-#     protocol = input(f'{i_protocol + 1} запись: ').split()
-#     name, score = protocol[1], int(protocol[0])
-#     if name in games:
-#         if games[name][0] < score:
-#             games[name] = [score, i_protocol + 1]
-#     else:
-#         games[name] = [score, i_protocol + 1]
-games = {'Jack': [95715, 6], 'qwerty': [197128, 5], 'Alex': [95715, 3], 'M': [95715, 9]}
-winner = {}
-
-for i, v in games.items():
-    i_score = v[0]
-    i_num = v[1]
-    sorted(games[i], key=winner_sort(i_score, i_num))
-print(winner)
-
-# TODO, необходимо произвести сортировку исходя из "количества очков" и "номера записи".
-#  Для этого, в key функции sorted необходимо передать функцию, которая будет принимать на вход
-#  "количества очков" и "номера записи", и возвращать какое-то число, исходя из которого можно произвести сортировку.
-#  Если позицию, к примеру 2, умножить на 100 000 000 и вычесть 95715, получим 199904285
-#  Если позицию, к примеру 3, умножить на 100 000 000 и вычесть 95715, получим 299904285
-#  Для того, чтобы победил тот, кто первый набрал большее количество очков, сортировку стоит делать от меньшего к большему.
+count_protocol = int(input('Сколько записей вносится в протокол? '))
+games = {}
 
 
+print('Записи (результат и имя): ')
+for i_protocol in range(count_protocol):
+    protocol = input(f'{i_protocol + 1} запись: ').split()
+    name, score = protocol[1], int(protocol[0])
+    if name in games:
+        if games[name][0] < score:
+            games[name] = [score, i_protocol + 1]
+    else:
+        games[name] = [score, i_protocol + 1]
 
+
+games_lst = list(games.items())
+games_lst.sort(key=winner_sort, reverse=True)
+
+
+print('Итоги соревнований:')
+for i_winner in range(3):
+    name = games_lst[i_winner][0]
+    score = games_lst[i_winner][1][0]
+    print(f'{i_winner + 1} место {name} - {score}')
 
 
 # 1 запись: 69485 Jack
@@ -49,10 +42,3 @@ print(winner)
 # 1 место. qwerty (197128)
 # 2 место. Alex (95715)
 # 3 место. Jack (95715)
-
-# TODO, предлагаю реализовать задание за 2 отдельных цикла.
-#  Один потребуется для запроса данных и заполнения словаря с данными.
-#  Второй для вывода данных.
-#  Словарь с данными стоит реализовать следующим образом
-#  "Ключ" (Имя): "Значение" (список ["Счёт", "Номер записи"]).
-#  Нам необходимы только записи с максимальным счётом у игрока.
