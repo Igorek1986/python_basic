0e4d70cd04720c0bfedea7d9b4e681a298a090cb
Insolente13 <lanc05@mail.ru>
2021-10-19 09:08:25 +0300

    Module 20 09 - TODO, another - ok

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -2,10 +2,9 @@ def my_len_min(string, data):
     return len(sorted((string, data), key=len)[0])
 
 
-word_str = 'abcd'
+word_str = 'abcdaaa'
 num = (10, 20, 30, 40)
 min_len = my_len_min(word_str, num)
-
 
 gen_obj = ((word_str[i], num[i]) for i in range(min_len))
 print(gen_obj)
@@ -13,10 +12,11 @@ print(gen_obj)
 for item in gen_obj:
     print(item)
 
-
 # sym_num = zip(word_str, num)
 # print(sym_num)
 #
 #
 # for i in sym_num:
 #     print(i)
+
+# зачёт!
