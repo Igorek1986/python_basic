9b2c4b8439fa2027dbdfcd27296b1e3412478860
Igor Ponomarev <9129910001@mail.ru>
2021-10-21 10:15:42 +0500

    Task 01, 02 done Task 03 question

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+def numbers(num):
+    if num != 1:
+        numbers(num - 1)
+    print(num)
+
+
+num_int = 5
+numbers(num_int)
--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def my_zip_min(string, data):
+    len_zip = len(sorted((string, data), key=len)[0])
+    zip_gen = ((string[i], data[i]) for i in range(len_zip))
+    return zip_gen
+
+
+word_str = 'abcdaaa'
+num = (10, 20, 30, 40)
+# num = {1: 10, 2: 20, 3: 30, 4: 40}
+gen_obj = my_zip_min(word_str, num)
+
+for i_tuple in gen_obj:
+    print(i_tuple)
