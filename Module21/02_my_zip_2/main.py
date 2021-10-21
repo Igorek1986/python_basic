cdb7e537e06fb42ef182fdfc40ca3ba511e8cf68
Igor Ponomarev <9129910001@mail.ru>
2021-10-21 21:20:15 +0500

    Task 02 update

--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -8,6 +8,8 @@ word_str = 'abcdaaa'
 num = (10, 20, 30, 40)
 # num = {1: 10, 2: 20, 3: 30, 4: 40}
 gen_obj = my_zip_min(word_str, num)
+# gen_obj = enumerate(word_str)
+
 
 for i_tuple in gen_obj:
     print(i_tuple)
