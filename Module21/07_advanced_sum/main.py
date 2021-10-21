5f003752722e2b1ddaeeac4107d98c84a23e83ff
Igor Ponomarev <9129910001@mail.ru>
2021-10-21 17:15:51 +0500

    Task 07, 08, 09 done

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def lst_open(lst):
+    result = []
+    for value in lst:
+        if isinstance(value, list):
+            result.extend(lst_open(value))
+        else:
+            result.append(value)
+    return result
+
+
+print('Ответ:', sum(lst_open([[1, 2, [3]], [1], 3])))
+print('Ответ:', sum(lst_open((1, 2, 3, 4, 5))))
--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,4 +1,15 @@
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+
+def lst_open(lst):
+    result = []
+    for value in lst:
+        if isinstance(value, list):
+            result.extend(lst_open(value))
+        else:
+            result.append(value)
+    return result
+
+
+print('Ответ:', lst_open(nice_list))
