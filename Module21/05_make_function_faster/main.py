aa969c1a17a9a132fd8db6b1450a6857bf424699
Igor Ponomarev <9129910001@mail.ru>
2021-10-21 13:37:45 +0500

    Task 04, 05 done

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,9 +1,16 @@
-def calculating_math_func(data):
+def calculating_math_func(data, fact_dct):
     result = 1
-    for index in range(1, data + 1):
-        result *= index
+    if data in fact_dct.keys():
+        result = fact_dct[data]
+    else:
+        for index in range(1, data + 1):
+            result *= index
+            fact_dct[index] = result
     result /= data ** 3
     result = result ** 10
     return result
 
-# TODO оптимизировать функцию
+
+factorial_dct = {}
+print(calculating_math_func(10, factorial_dct))
+print(calculating_math_func(5, factorial_dct))
