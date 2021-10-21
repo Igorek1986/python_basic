248efad0145d2198a7c1ab1d0861a874733aa122
Igor Ponomarev <9129910001@mail.ru>
2021-10-21 10:15:42 +0500

    Task 01, 03 done Task 02 question

--- Module21/03_fibonacci/main.py
+++ Module21/03_fibonacci/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def fibonacci(num):
+    if num in (1, 2):
+        return 1
+    else:
+        return fibonacci(num - 1) + fibonacci(num - 2)
+
+
+number_row = int(input('Введите позицию числа в ряде Фибоначчи: '))
+print('Число:', fibonacci(number_row))
