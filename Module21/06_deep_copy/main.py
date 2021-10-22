a8738024bf9f695f86b0056636f061ca8c5130a0
Igor Ponomarev <9129910001@mail.ru>
2021-10-22 10:15:02 +0500

    Task 07 update, 06 done

--- Module21/06_deep_copy/main.py
+++ Module21/06_deep_copy/main.py
@@ -1,3 +1,18 @@
+import copy
+
+
+def copy_site(structure, count=0, n_site={}):
+    for _ in range(count):
+        name = input('Введите название продукта для нового сайта: ')
+        if isinstance(structure, dict):
+            key = 'Сайт для ' + name
+            n_site[key] = copy.deepcopy(structure)
+            n_site[key]['html']['head']['title'] = 'Куплю/продам ' + name + ' недорого'
+            n_site[key]['html']['body']['h2'] = 'У нас самая низкая цена на ' + name
+            print(n_site)
+    return n_site
+
+
 site = {
     'html': {
         'head': {
@@ -11,4 +26,6 @@ site = {
     }
 }
 
-# TODO здесь писать код
+
+count_site = int(input('Сколько сайтов: '))
+copy_site(site, count_site)
--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -10,14 +10,10 @@
 
 def lst_open(lst):
     lst_sum = 0
-    count = len(lst)
-    if count == 0:
-        return 1
     for value in lst:
         if isinstance(value, list):
             lst_sum += lst_open(value)
         else:
-            count -= 1
             lst_sum += value
     return lst_sum
 
