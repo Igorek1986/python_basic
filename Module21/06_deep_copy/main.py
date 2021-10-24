9f551a73a270941679c3243309e5c03e579f75dd
Igor Ponomarev <9129910001@mail.ru>
2021-10-24 18:51:30 +0500

    Task 06 question

--- Module21/06_deep_copy/main.py
+++ Module21/06_deep_copy/main.py
@@ -13,24 +13,29 @@ def find_key(structure, key, k_value):
 def structure_print(s, space=0):
     for i_key, i_value in s.items():
         if isinstance(i_value, dict):
-            print(' ' * space, f'{i_key}: {{')
-            space += 4
-            structure_print(i_value, space)
-            space -= 4
+            print(' ' * space, f"'{i_key}': {{")
+            structure_print(i_value, space + 4)
+            print(' ' * space, "}")
         else:
-            print(' ' * space, f'{i_key}: {i_value}')
+            print(' ' * space, f"'{i_key}': '{i_value}',")
 
 
-def copy_site(structure, key_title, key_h2, count=0, n_site={}):
+def copy_site(structure, key_title, key_h2, lst, count=0, n_site={}):
     for _ in range(count):
         name = input('Введите название продукта для нового сайта: ')
         if isinstance(structure, dict):
-            # key = 'Сайт для ' + name
-            key = 'site ='
+            key = 'Сайт для ' + name
+            # key = 'site ='
+            # key = ''
+            # n_site = copy.deepcopy(structure)
+            # find_key(n_site, key_title, k_value=f'Куплю/продам {name} недорого')
+            # lst.append(find_key(n_site, key_h2, k_value=f'У нас самая низкая цена на {name}'))
             n_site[key] = copy.deepcopy(structure)
             find_key(n_site[key], key_title, k_value=f'Куплю/продам {name} недорого')
-            find_key(n_site[key], key_h2, k_value=f'У нас самая низкая цена на {name}')
-            print(f'Сайт для {name}:')
+            lst.append(find_key(n_site[key], key_h2, k_value=f'У нас самая низкая цена на {name}'))
+            # print(f'Сайт для {name}:')
+            print('site = {')
+            # print(lst)
             structure_print(n_site)
 
 
@@ -47,7 +52,9 @@ site = {
     }
 }
 
+site_lst = []
+
 count_site = int(input('Сколько сайтов: '))
-copy_site(site, 'title', 'h2', count_site)
+copy_site(site, 'title', 'h2', site_lst, count_site)
 
 # зачёт!
