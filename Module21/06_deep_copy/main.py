16cb860ac347135e1d43efd68ff617bff98a1b08
Igor Ponomarev <9129910001@mail.ru>
2021-10-24 20:35:10 +0500

    Task 06 doen

--- Module21/06_deep_copy/main.py
+++ Module21/06_deep_copy/main.py
@@ -24,24 +24,17 @@ def copy_site(structure, key_title, key_h2, lst, count=0, n_site={}):
     for _ in range(count):
         name = input('Введите название продукта для нового сайта: ')
         if isinstance(structure, dict):
-            key = 'Сайт для ' + name
-            # key = 'site ='
-            # key = ''
+            lst.append(name)
             n_site = copy.deepcopy(structure)
             find_key(n_site, key_title, k_value=f'Куплю/продам {name} недорого')
             find_key(n_site, key_h2, k_value=f'У нас самая низкая цена на {name}')
-            # n_site[key] = copy.deepcopy(structure)
-            # find_key(n_site[key], key_title, k_value=f'Куплю/продам {name} недорого')
-            # lst.append(find_key(n_site[key], key_h2, k_value=f'У нас самая низкая цена на {name}'))
-            print(f'Сайт для {name}:')
-            # print('site = {')
             lst.append(n_site)
-            # structure_print(n_site)
-            for i in range(len(lst)):
+            for i in range(1, len(lst) + 1, 2):
+                name_site = lst[i - 1]
+                print(f'Сайт для {name_site}:')
                 print('site = {')
                 structure_print(lst[i])
-            # lst.append(n_site)
-            # print(lst)
+            print('}')
 
 
 site = {
@@ -57,8 +50,8 @@ site = {
     }
 }
 
-site_lst = []
 
+site_lst = []
 count_site = int(input('Сколько сайтов: '))
 copy_site(site, 'title', 'h2', site_lst, count_site)
 
