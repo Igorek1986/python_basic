bd31b27fb67d3dc6e5bd19a5c8a80b0b401d383f
Igor Ponomarev <9129910001@mail.ru>
2021-10-26 10:21:12 +0500

    Task 01, 02, 03 done

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+lst_numbers = []
+file_input = open('numbers.txt', 'r', encoding='utf-8')
+file_answer = open('answer.txt', 'w', encoding='utf-8')
+print('Содержимое файла numbers.txt')
+for i_line in file_input:
+    print(i_line, end='')
+    num = i_line.strip()
+    if num.isdigit():
+        lst_numbers.append(int(num))
+
+
+file_answer.write(str(sum(lst_numbers)))
+file_answer.close()
+file_input.close()
+
+
+file_answer = open('answer.txt', 'r')
+print('Содержимое файла answer.txt')
+for i_line in file_answer:
+    print(i_line, end='')
+file_answer.close()
--- Module22/03_zen_of_python_2/main.py
+++ Module22/03_zen_of_python_2/main.py
@@ -1,1 +1,48 @@
-# TODO здесь писать код
+import os
+
+
+def find_file(cur_path, search_obj):
+    if os.path.exists(cur_path):
+        if os.path.isdir(cur_path):
+            for i_elem in os.listdir(cur_path):
+                path = os.path.join(cur_path, i_elem)
+                if i_elem == search_obj:
+                    return path
+                else:
+                    result = find_file(path, search_obj)
+                    if result:
+                        break
+            else:
+                result = None
+            return result
+    else:
+        print('Файла в данной директории нет')
+
+
+def count_alphabet(cur_path, count=0, lines=0, words=0):
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' \
+               'abcdefghijklmnopqrstuvwxyz'
+    file = open(cur_path, 'r')
+    for i_line in file:
+        count += sum(1 for alpha in i_line if alpha in alphabet)
+        lines += 1
+        flag = True
+        for letter in i_line:
+            if letter != ' ' and flag:
+                words += 1
+                flag = False
+            else:
+                flag = True
+    file.close()
+    return count, lines, words
+
+
+path_search = os.path.abspath(os.path.join('..'))
+file_name = 'zen.txt'
+
+
+result_path = find_file(path_search, file_name)
+count_alp = count_alphabet(result_path)
+print('Количество букв:', count_alp[0])
+print('Количество линий:', count_alp[1])
+print('Количество слов:', count_alp[2])
