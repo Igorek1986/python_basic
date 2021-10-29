e735bbbe259623e0ef4b4b98bf5693799999f914
Igor Ponomarev <9129910001@mail.ru>
2021-10-29 19:57:56 +0500

    Task 07 question

--- Module22/07_tournament/main.py
+++ Module22/07_tournament/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+def sorted_tour(string):
+    string = string.split()
+    name = str(string[-2][:1]) + '.'
+    surname = str(string[0])
+    score = str(string[-1])
+    return ' '.join((name, surname, score))
+
+
+
+data = []
+with open('first_tour.txt', 'r') as file:
+    min_score = file.readline()
+    for i_line in file:
+        if not i_line.split()[-1] < min_score:
+            sort_score = sorted_tour(i_line)
+            data.append(sort_score)
+
+
+
+
+    print(data)
