4c4f8a0bc6ec8d8dc969ec7bebb21049291cc31a
Igor Ponomarev <9129910001@mail.ru>
2021-10-31 17:38:37 +0500

    Task 07 update, 08 done

--- Module22/07_tournament/main.py
+++ Module22/07_tournament/main.py
@@ -4,7 +4,7 @@ def winners_tour(string):
         name = str(string[i][1][:1]) + '.'
         surname = str(string[i][0])
         score = str(string[i][-1])
-        string[i] = ' '.join((name, surname, score + '\n'))
+        string[i] = ''.join(f'{i+1}) {name} {surname} {score} \n')
 
 
 data = []
@@ -16,9 +16,11 @@ with open('first_tour.txt', 'r') as file:
 
 data.sort(key=lambda key: key[2], reverse=True)
 winners_tour(data)
-print(data)
-with open('second_tour.txt', 'w') as file_winners:
+print('Содержимое файла second_tour.txt:')
+with open('second_tour.txt', 'w+') as file_winners:
+    file_winners.write(str(len(data)) + '\n')
     file_winners.writelines(data)
-
-# TODO, пожалуйста, обратите внимание на условие задания
-#  "В первой строке нужно вывести в файл second_tour.txt количество участников второго тура. "
+    file_winners.seek(0)
+    print(*file_winners)
+    # for i_line in file_winners:
+    #     print(i_line, end='')
--- Module22/08_frequency_analysis/main.py
+++ Module22/08_frequency_analysis/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+def count_letter_text(file, count=0, cont_letter_dict={}):
+    for i_line in file:
+        for alpha in i_line:
+            if alpha.isalpha():
+                count += 1
+        for letter in i_line.lower():
+            if letter.isalpha() and letter in cont_letter_dict:
+                cont_letter_dict[letter] += 1
+            elif letter.isalpha():
+                cont_letter_dict[letter] = 1
+    cont_letter_dict = dict(sorted(cont_letter_dict.items(), key=lambda items: (items[1] * -1, items[0])))
+    return count, cont_letter_dict
+
+
+with open('text.txt', 'r') as text:
+    total_letter, count_letter_analysis = count_letter_text(text)
+    for key, value in count_letter_analysis.items():
+        analysis_letter = round(value / total_letter, 3)
+        file_answer = open('analysis.txt', 'a+')
+        file_answer.write(f'{key}: {analysis_letter} \n')
+    file_answer.seek(0)
+    print('Содержимое файла analysis.txt:')
+    print(*file_answer)
+    file_answer.close()
