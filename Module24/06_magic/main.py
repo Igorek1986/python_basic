9e642e55fbbc8273c4a8114095328e0a112b575f
Igor Ponomarev <9129910001@mail.ru>
2021-11-15 22:06:24 +0500

    Task 06 question

--- Module24/06_magic/main.py
+++ Module24/06_magic/main.py
@@ -1,1 +1,88 @@
-# TODO здесь писать код
+class Water:
+
+    def __str__(self):
+        return 'Вода'
+
+    def __init__(self, name='Вода'):
+        self.name = 'Вода'
+
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Storm()
+        elif isinstance(other, Fire):
+            return Steam()
+        elif isinstance(other, Earth):
+            return Dirt()
+        return None
+
+
+class Air:
+
+    def __str__(self):
+        return 'Воздух'
+
+    def __add__(self, other):
+        if isinstance(other, Fire):
+            return Lightning()
+        if isinstance(other, Earth):
+            return Dust()
+        return None
+
+
+class Fire:
+
+    def __str__(self):
+        return 'Огонь'
+
+    def __add__(self, other):
+        if isinstance(other, Earth):
+            return Lava()
+        return None
+
+
+class Earth:
+
+    def __str__(self):
+        return 'Земля'
+
+
+class Storm:
+
+    def __str__(self):
+        return 'Шторм'
+
+    def __init__(self, name='Шторм'):
+        self.name = 'Шторм'
+
+
+class Steam:
+
+    def __str__(self):
+        return 'Пар'
+
+
+class Dirt:
+
+    def __str__(self):
+        return 'Грязь'
+
+
+class Lightning:
+
+    def __str__(self):
+        return 'Молния'
+
+
+class Dust:
+
+    def __str__(self):
+        return 'Пыль'
+
+
+class Lava:
+
+    def __str__(self):
+        return 'Лава'
+
+
+print(Water() + Air())
