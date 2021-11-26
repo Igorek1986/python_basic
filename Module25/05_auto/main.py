4b535b0d78adf27459f8b103714c3a72477d328f
Igor Ponomarev <9129910001@mail.ru>
2021-11-26 20:10:15 +0500

    Task 05 question

--- Module25/05_auto/main.py
+++ Module25/05_auto/main.py
@@ -1,1 +1,30 @@
-# TODO здесь писать код
+import math
+
+
+class Car:
+    def __init__(self, x, y, angel, distance):
+        self.x = 0
+        self.y = 0
+        self.angel = angel
+        self.distance = distance
+
+    def move(self, distance, angel):
+        self.x = round(self.x + distance * math.sin(math.radians(angel)))
+        self.y = round(self.x + distance * math.cos(math.radians(angel)))
+
+
+class Bus(Car):
+    def __init__(self, x, y, angel, distance):
+        super().__init__(x, y, angel, distance)
+        self.count_of_passengers = 0
+        self.cash = 0
+
+    def in_the_bus(self, count):
+        self.count_of_passengers += count
+
+    def from_the_bus(self, count):
+        self.count_of_passengers -= count
+
+    def move(self, distance, angel):
+        self.x = round(self.x + distance * math.sin(math.radians(angel)))
+        self.y = round(self.x + distance * math.cos(math.radians(angel)))
