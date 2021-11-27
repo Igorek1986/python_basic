83ec665d4440bd26a1db0a48089e788da721a4d9
Insolente13 <lanc05@mail.ru>
2021-11-27 10:06:22 +0300

    Module 25 02, 05 - TODO, 01, 03 - ok

--- Module25/01_taxes/main.py
+++ Module25/01_taxes/main.py
@@ -1,6 +1,5 @@
 from property import Apartment, Car, CountryHouse
 
-
 apartment = Apartment(worth=6000000, name='Квартира')
 car = Car(worth=2500000, name='Машина')
 country_house = CountryHouse(worth=1500000, name='Дача')
@@ -8,3 +7,5 @@ country_house = CountryHouse(worth=1500000, name='Дача')
 print(apartment)
 print(car)
 print(country_house)
+
+# зачёт!
--- Module25/02_karma/karma.py
+++ Module25/02_karma/karma.py
@@ -15,6 +15,10 @@ class Karma:
             carma_day = random.randint(1, 7)
             self.carma_count += carma_day
         else:
+            # TODO, запись данных в файл в этом методе получилась лишней.
+            #  Метод должен или увеличивать количество кармы, или вызывать одно из исключений при помощи raise.
+            #  Сам метод стоит запускать внутри блоков try/except, таким образом, запись исключений в файл,
+            #  мы сможем производить в блоке except.
             with open('karma.log', 'a', encoding='utf-8') as file:
                 file.write(f'Исключение возникло на {self.day} день просвещения' +
                            str(random.choice(self.my_exception)) + '\n')
