be1b545e72717dc4374f93eba9dbea35ed854bab
Igor Ponomarev <9129910001@mail.ru>
2021-11-30 22:52:48 +0500

    Task 07 sync

--- Module25/07_stack/main.py
+++ Module25/07_stack/main.py
@@ -7,6 +7,9 @@ class Stack:
     def del_stack(self):
         del self.stack[-1]
 
+    def __str__(self):
+        return f'{self.stack}'
+
 
 class TaskManager:
     task_manager = {}
@@ -15,14 +18,19 @@ class TaskManager:
         pass
 
     def new_task(self, task, num):
-        self.task_manager[task] = num
+        print(num)
+        if num not in self.task_manager.keys():
+            Stack.add_stack(Stack(), f'{num} {task}')
+        else:
+            self.task_manager[num] = Stack()
 
 
 manager = TaskManager()
 manager.new_task("сделать уборку", 4)
 manager.new_task("помыть посуду", 4)
-manager.new_task("отдохнуть", 1)
-manager.new_task("поесть", 2)
-manager.new_task("сдать дз", 2)
+# manager.new_task("отдохнуть", 1)
+# manager.new_task("поесть", 2)
+# manager.new_task("сдать дз", 2)
+print(Stack())
 
-print(manager)
+# print(manager)
