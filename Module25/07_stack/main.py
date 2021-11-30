f78b4de8df0f3977af28931085073552c4e3724a
Igor Ponomarev <9129910001@mail.ru>
2021-11-30 21:23:12 +0500

    Task 07 question

--- Module25/07_stack/main.py
+++ Module25/07_stack/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+class Stack:
+    stack = []
+
+    def add_stack(self, obj):
+        self.stack.append(obj)
+
+    def del_stack(self):
+        del self.stack[-1]
+
+
+class TaskManager:
+    task_manager = {}
+
+    def __str__(self):
+        pass
+
+    def new_task(self, task, num):
+        self.task_manager[task] = num
+
+
+manager = TaskManager()
+manager.new_task("сделать уборку", 4)
+manager.new_task("помыть посуду", 4)
+manager.new_task("отдохнуть", 1)
+manager.new_task("поесть", 2)
+manager.new_task("сдать дз", 2)
+
+print(manager)
