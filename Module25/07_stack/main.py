065e35146f4b335ad05cdbc6411648d41c74f3e4
Igor Ponomarev <9129910001@mail.ru>
2021-12-02 05:47:07 +0500

    Task 07 parsing homework

--- Module25/07_stack/main.py
+++ Module25/07_stack/main.py
@@ -2,13 +2,15 @@ from stack import Stack
 
 
 class TaskManager:
-    # TODO, т.к. этот список относится только к одному объекту класса, стоит реализовать его в метода __init__.
-    task_manager = {}
+    def __init__(self):
+        self.task_manager = {}
 
     def __str__(self):
-        # TODO, после того, как метод __str__ класса Stack будет реализован,
-        #  Предлагаю создать цикл по словарю task_manager, внутри которого, необходимо собрать строку для возврата при помощи return.
-        return ''
+        display = []
+        if self.task_manager:
+            for i_num in sorted(self.task_manager.keys()):
+                display.append(f'{i_num} {self.task_manager[i_num]}\n')
+        return ''.join(display)
 
     def new_task(self, task, num):
 
@@ -24,8 +26,5 @@ manager.new_task("отдохнуть", 1)
 manager.new_task("поесть", 2)
 manager.new_task("сдать дз", 2)
 
-# TODO, цикл ниже получился лишним
-for i in Stack.stack:
-    print(i)
 
 print(manager)
