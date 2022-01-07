5ffaeaad6b5406131cf53f9bf83cb604c835ffcd
Igor Ponomarev <9129910001@mail.ru>
2022-01-07 18:58:58 +0500

    module 28 sync

--- Module28/03_modeling/main.py
+++ Module28/03_modeling/main.py
@@ -1,8 +1,38 @@
-class Square:
+from abc import ABC, abstractmethod
 
-    """" Базовый класс квадрата """
+
+class Figure(ABC):
+
+    """ Абстрактынй базовый класс фигуры"""
+
+    @abstractmethod
+    def __init__(self, side_1: int, side_2: int):
+        self._side_1 = side_1
+        self._side_2 = side_2
+
+    @property
+    def side_1(self):
+        return self._side_1
+
+    @side_1.setter
+    def side_1(self, side_1):
+        self._side_1 = side_1
+
+    @property
+    def side_2(self):
+        return self._side_2
+
+    @side_2.setter
+    def side_2(self, side_2):
+        self._side_2 = side_2
+
+
+class Square(Figure):
+
+    """" Дочерний класс квадрата от Фигуры """
 
     def __init__(self, side: int):
+        super().__init__(side_1=side, side_2=side)
         self.side = side
 
     def perimetr(self) -> int:
@@ -12,11 +42,12 @@ class Square:
         return self.side ** 2
 
 
-class Triangle:
+class Triangle(Figure):
 
-    """ Базовый класс треугольника """
+    """ Дочерний класс треугольника от Фигуры """
 
     def __init__(self, base: int, height: int):
+        super().__init__(side_1=base, side_2=height)
         self.base = base
         self.height = height
 
@@ -41,4 +72,7 @@ class Triangle3D(Triangle):
 
 
 a = Square(side=7)
+a.side_1 = 8
 print(a.square())
+b = Triangle(base=6, height=2)
+print(b.square())
