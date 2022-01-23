c13b8b543621b01acb8f7c9c73118933035ae4b5
Igor Ponomarev <9129910001@mail.ru>
2022-01-23 16:29:57 +0500

    Task 01 done. 02 question

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,1 +1,41 @@
-# TODO здесь писать код
+from collections.abc import Callable
+import functools
+
+
+def check_permission(name: str = 'user') -> Callable:
+    """ Декоратор проверки прав доступа"""
+
+    def decorator_func(func: Callable) -> Callable:
+
+        @functools.wraps(func)
+        def wrapper_check(*args, **kwargs):
+            try:
+                if name == 'admin':
+                    return func(*args, **kwargs)
+                raise PermissionError('PermissionError')
+            except PermissionError as exc:
+                print(f'{exc}: У пользователя недостаточно прав, чтобы выполнить функцию add_comment')
+        return wrapper_check
+    return decorator_func
+
+
+user_permissions = ['admin']
+
+
+@check_permission('admin')
+def delete_site():
+    print('Удаляем сайт')
+
+
+@check_permission('user_1')
+def add_comment():
+    print('Добавляем комментарий')
+
+
+delete_site()
+add_comment()
+
+
+# Результат:
+# Удаляем сайт
+# PermissionError: У пользователя недостаточно прав, чтобы выполнить функцию add_comment
--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,32 @@
-# TODO здесь писать код
+from typing import Callable
+import functools
+app = {}
+
+
+def callback(key: str) -> Callable:
+    def call_f(func):
+        @functools.wraps(func)
+        def wrapper():
+            app[key] = func
+        return wrapper
+    return call_f
+
+
+@callback('//')
+def example():
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+route = app.get('//')
+
+if route:
+    response = route()
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
+
+
+# Ожидаемый результат:
+# Пример функции, которая возвращает ответ сервера
+# Ответ: OK
