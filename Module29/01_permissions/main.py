fe0e57fb4d5dfffd19cc80204ba00f9d6a95a9d3
Insolente13 <lanc05@mail.ru>
2022-01-25 12:54:54 +0300

    Module 29 01, 03 - ok

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,7 +1,6 @@
 from collections.abc import Callable
 import functools
 
-
 user_permissions = ['admin']
 
 
@@ -9,13 +8,14 @@ def check_permission(name: str = 'user') -> Callable:
     """ Декоратор проверки прав доступа"""
 
     def decorator_func(func: Callable) -> Callable:
-
         @functools.wraps(func)
         def wrapper_check(*args, **kwargs):
             if name in user_permissions:
                 return func(*args, **kwargs)
             raise PermissionError('У пользователя недостаточно прав, чтобы выполнить функцию add_comment')
+
         return wrapper_check
+
     return decorator_func
 
 
@@ -32,7 +32,8 @@ def add_comment():
 delete_site()
 add_comment()
 
-
 # Результат:
 # Удаляем сайт
 # PermissionError: У пользователя недостаточно прав, чтобы выполнить функцию add_comment
+
+# зачёт!
